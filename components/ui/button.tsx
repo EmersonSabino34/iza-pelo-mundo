646134f4533b653bbd555188460a9a1ca808def7
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-11 17:08:49 +0000

    Update button.tsx

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
-
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   children: React.ReactNode;
   variant?: "primary" | "ghost" | "outline";
