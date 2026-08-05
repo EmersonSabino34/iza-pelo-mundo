d841a7d3c21a17a8d087d408ddaefd71a128d110
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-08-05 21:48:22 +0100

    Atualizar o card.tsx

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 
 type CardProps = React.HTMLAttributes<HTMLDivElement> & {
   children: React.ReactNode;
+
 };
 export const Card = forwardRef<HTMLDivElement, CardProps>(
   ({ children, className = "", ...props }, ref) => {
